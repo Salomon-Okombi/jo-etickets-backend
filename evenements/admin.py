60067fafea2bc507ec41765141c4d683c667d711
Salomon-Okombi <salomon.okombi@gmail.com>
2025-10-01 17:10:40 +0200

    Maj offres(model et admin.py). correction du bug createur_id.

--- evenements/admin.py
+++ evenements/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Evenement
 
-'''
 @admin.register(Evenement)
 class EvenementAdmin(admin.ModelAdmin):
-    list_display = ("nom", "discipline", "date_evenement", "lieu", "statut")
-    list_filter = ("discipline", "statut")
-    search_fields = ("nom", "discipline", "lieu")
-'''
+    list_display = ('nom', 'discipline_sportive', 'date_evenement', 'lieu_evenement', 'statut', 'date_creation')
+    list_filter = ('statut', 'discipline_sportive', 'date_evenement')
+    search_fields = ('nom', 'discipline_sportive', 'lieu_evenement')
+    ordering = ('date_evenement',)
+    date_hierarchy = 'date_evenement'
