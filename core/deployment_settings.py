63814e154d6445f89c870226f0faa0bbafc1fc69
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-03-25 15:14:37 +0100

    deployment setting updated

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -4,10 +4,10 @@ from .settings import *
 from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
-CSRF_TRSTED_ORIGINS = ["https://"+os.environ.get["RENDER_EXTERNAL_HOSTNAME"]]
+CSRF_TRSTED_ORIGINS = ["https://"+os.environ.get("RENDER_EXTERNAL_HOSTNAME")]
 
 DEBUG = False
-SECRET_KEY = os.environ.get["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # =========================================================
