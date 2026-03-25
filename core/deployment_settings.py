3eb38557471474c7e89db2c4b61cd6b4416dfb49
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-03-25 16:11:16 +0100

    deployment setting updated-4

--- core/deployment_settings.py
+++ core/deployment_settings.py
@@ -43,6 +43,18 @@ STORAGES = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
+
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
