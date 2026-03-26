aae2a0f46295aa902a259866d612380deee95e08
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-03-26 21:04:41 +0100

    fix biuld.sh : add space between []

--- build.sh
+++ build.sh
@@ -1,12 +1,20 @@
+#!/usr/bin/env bash
 set -o errexit
 
+# Installer les dépendances
 pip install -r requirements.txt
 
+# Static files
 python manage.py collectstatic --no-input
 
+# Migrations
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]];
-then
-    python manage.py createsuperuser --no-input
+# Création automatique du superuser (si activée)
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    echo "Creating Django superuser..."
+
+    python manage.py createsuperuser --no-input || \
+    echo "Superuser already exists, skipping."
 fi
+``
