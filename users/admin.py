a605f70f40c5ec3a9bc2ec22098220a17d0cbd06
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-04-03 22:18:16 +0200

    Fix affichage de billets 5

--- users/admin.py
+++ users/admin.py
@@ -1,84 +1,86 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Utilisateur, HistoriqueConnexion
 
 
 @admin.register(Utilisateur)
-class UtilisateurAdmin(UserAdmin):
+class UtilisateurAdminDebug(UserAdmin):
+    """
+    ADMIN TEMPORAIRE DE DEBUG
+     À SUPPRIMER APRÈS CORRECTION DU ROOT
+    """
 
     list_display = (
+        "id",
         "username",
         "email",
         "role",
-        "est_verifie",
-        "is_active",
-        "date_joined",
-        "last_login",
-        "tentatives_connexion",
-    )
-
-    list_filter = (
-        "role",
-        "est_verifie",
         "is_staff",
         "is_superuser",
         "is_active",
     )
 
-    search_fields = (
-        "username",
-        "email",
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_superuser",
+        "is_active",
     )
 
-    ordering = ("-date_joined",)
+    search_fields = ("username", "email")
 
-    # ✅ On n’ajoute QUE les champs métiers
-    fieldsets = UserAdmin.fieldsets + (
-        ("Informations supplémentaires", {
+    ordering = ("-id",)
+
+    # On affiche TOUTES LES CHOSES UTILES
+    fieldsets = (
+        (None, {
             "fields": (
-                "role",
+                "username",
+                "password",
+            )
+        }),
+        ("Informations personnelles", {
+            "fields": (
+                "email",
+                "first_name",
+                "last_name",
                 "telephone",
                 "photo_profil",
+            )
+        }),
+        ("Droits", {
+            "fields": (
+                "role",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        ("Sécurité / état", {
+            "fields": (
                 "est_verifie",
                 "est_bloque",
                 "tentatives_connexion",
                 "derniere_connexion_ip",
+                "last_login",
+                "date_joined",
             )
         }),
     )
 
     readonly_fields = (
-        "date_joined",
         "last_login",
+        "date_joined",
     )
 
 
 @admin.register(HistoriqueConnexion)
 class HistoriqueConnexionAdmin(admin.ModelAdmin):
-
     list_display = (
         "utilisateur",
         "date_connexion",
-        "adresse_ip",
         "statut_connexion",
         "type_action",
-    )
-
-    list_filter = (
-        "statut_connexion",
-        "type_action",
-        "date_connexion",
-    )
-
-    search_fields = (
-        "utilisateur__username",
-        "utilisateur__email",
-        "adresse_ip",
-    )
-
-    ordering = ("-date_connexion",)
-
-    readonly_fields = (
-        "date_connexion",
     )
