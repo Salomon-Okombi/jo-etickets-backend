9f7fb30ac26ad3b07f2f967699330723956be64f
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-04-06 18:11:15 +0200

    refonte event detail page 5

--- evenements/views.py
+++ evenements/views.py
@@ -1,20 +1,20 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import AllowAny
+from rest_framework.authentication import SessionAuthentication
 from .models import Evenement
-from .serializers import (
-    EvenementListSerializer,
-    EvenementDetailSerializer,
-)
+from .serializers import EvenementListSerializer, EvenementDetailSerializer
 
 
 class EvenementViewSet(ReadOnlyModelViewSet):
     """
-    API publique.
-    Utilisée par la boutique événements (visiteurs + clients).
-    Lecture seule.
+    API publique – Boutique événements
+    Aucune authentification requise.
     """
 
+    #  NE PAS laisser JWT s’exécuter
+    authentication_classes = [SessionAuthentication]
     permission_classes = [AllowAny]
+
     queryset = Evenement.objects.filter(statut="PUBLIE")
 
     def get_serializer_class(self):
