5fc14122ae89a72b826e561c3a3e893921dc03f3
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-04-06 21:51:41 +0200

    fix event display 9

--- evenements/serializers.py
+++ evenements/serializers.py
@@ -3,63 +3,7 @@ from rest_framework import serializers
 from .models import Evenement
 
 
-class EvenementListSerializer(serializers.ModelSerializer):
-    """
-    Serializer public (liste)
-    Utilisé pour la boutique
-    """
-    image_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Evenement
-        fields = [
-            "id",
-            "nom_evenement",
-            "discipline",
-            "date_evenement",
-            "lieu",
-            "description_courte",
-            "image_url",
-        ]
-
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-
-
-class EvenementDetailSerializer(serializers.ModelSerializer):
-    """
-    Serializer public (détail)
-    """
-    image_url = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Evenement
-        fields = [
-            "id",
-            "nom_evenement",
-            "discipline",
-            "date_evenement",
-            "lieu",
-            "description_longue",
-            "image_url",
-        ]
-
-    def get_image_url(self, obj):
-        request = self.context.get("request")
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
-
-
 class EvenementAdminSerializer(serializers.ModelSerializer):
-    """
-    Serializer ADMIN
-    Autorise création / édition complète (avec image)
-    """
-
     class Meta:
         model = Evenement
         fields = [
--- evenements/views_admin.py
+++ evenements/views_admin.py
@@ -1,15 +1,18 @@
 # evenements/views_admin.py
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
+from rest_framework.parsers import MultiPartParser, FormParser
+
 from .models import Evenement
 from .serializers import EvenementAdminSerializer
 
 
 class EvenementAdminViewSet(ModelViewSet):
     """
-    API ADMIN
+    API ADMIN EVENEMENTS
     /api/evenements/admin/
     """
     queryset = Evenement.objects.all().order_by("-date_creation")
     serializer_class = EvenementAdminSerializer
     permission_classes = [IsAdminUser]
+    parser_classes = [MultiPartParser, FormParser]
