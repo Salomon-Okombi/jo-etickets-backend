65fbbdb717a2b9f67b5072193085d0b84dbf174e
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-04-06 19:08:59 +0200

    fix event display

--- evenements/urls.py
+++ evenements/urls.py
@@ -3,7 +3,19 @@ from .views import EvenementViewSet
 from .views_admin import EvenementAdminViewSet
 
 router = DefaultRouter()
-router.register("evenements", EvenementViewSet, basename="evenements")
-router.register("admin/evenements", EvenementAdminViewSet, basename="admin-evenements")
+
+#  PUBLIC
+router.register(
+    r"evenements",
+    EvenementViewSet,
+    basename="evenements",
+)
+
+# ADMIN
+router.register(
+    r"admin/evenements",
+    EvenementAdminViewSet,
+    basename="admin-evenements",
+)
 
 urlpatterns = router.urls
