7ae86c8ae74364def9f1075231e0d2798b999814
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-04-09 18:50:27 +0200

    fix event display image 4

--- evenements/serializers_public.py
+++ evenements/serializers_public.py
@@ -1,16 +1,45 @@
-# evenements/serializers_public.py
 from rest_framework import serializers
-from evenements.models import Evenement
+from django.conf import settings
+from .models import Evenement
 
-class PublicEvenementSerializer(serializers.ModelSerializer):
+
+class EvenementListSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Evenement
         fields = [
             "id",
-            "nom",
-            "discipline_sportive",
+            "nom_evenement",
+            "discipline",
             "date_evenement",
-            "lieu_evenement",
-            "description",
-            "statut",
+            "lieu",
+            "description_courte",
+            "image_url",
         ]
+
+    def get_image_url(self, obj):
+        if not obj.image:
+            return None
+        return f"{settings.MEDIA_URL}{obj.image.name}"
+
+
+class EvenementDetailSerializer(serializers.ModelSerializer):
+    image_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Evenement
+        fields = [
+            "id",
+            "nom_evenement",
+            "discipline",
+            "date_evenement",
+            "lieu",
+            "description_longue",
+            "image_url",
+        ]
+
+    def get_image_url(self, obj):
+        if not obj.image:
+            return None
+        return f"{settings.MEDIA_URL}{obj.image.name}"
