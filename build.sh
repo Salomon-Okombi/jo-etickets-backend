33b4bf87aed061277053a73ce9d7c2932ae5249f
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-05-19 01:10:54 +0200

    fix stop socket 6

--- build.sh
+++ build.sh
@@ -5,6 +5,19 @@ export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 pip install -r requirements.txt
 
+# Migrations
+python manage.py migrate
+python manage.py shell < scripts/create_superuser.py
+
+# Création automatique du superuser (si activée)
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+    echo "Creating Django superuser..."
+
+    python manage.py createsuperuser --no-input || \
+    echo "Superuser already exists, skipping."
+fi
+``
+
 #  créer dossier logs
 mkdir -p logs
 
