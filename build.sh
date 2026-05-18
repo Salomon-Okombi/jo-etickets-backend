0759c82ecc6e3c0c2c359d094488154e33f3542a
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-05-18 16:30:23 +0200

    adding of start.sh file

--- build.sh
+++ build.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Aller dans le dossier Django (si ton manage.py est dans backend/)
+cd backend
+
 # Installer les dépendances
 pip install -r requirements.txt
 
 # Static files
-python manage.py collectstatic --no-input
-
-# Migrations
-python manage.py migrate
-python manage.py shell < scripts/create_superuser.py
-
-# Création automatique du superuser (si activée)
-if [ "$CREATE_SUPERUSER" = "true" ]; then
-    echo "Creating Django superuser..."
-
-    python manage.py createsuperuser --no-input || \
-    echo "Superuser already exists, skipping."
-fi
-``
+python manage.py collectstatic --noinput
