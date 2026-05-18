d2ac78e19e6e5e0dba22376e89e82fc730301474
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-05-18 17:35:52 +0200

    adding of start.sh file 2

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# IMPORTANT : ajoute ça
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
-# Installer les dépendances
 pip install -r requirements.txt
 
-# Static
+# ✅ créer dossier logs
+mkdir -p logs
+
+# ✅ static
 python manage.py collectstatic --noinput
