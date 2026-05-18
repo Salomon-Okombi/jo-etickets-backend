2ad8f8ba8637414fbe75f729859ef99a25c1dcdf
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-05-18 16:40:10 +0200

    adding of start.sh file

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Aller dans le dossier Django (si ton manage.py est dans backend/)
-cd backend
-
 # Installer les dépendances
 pip install -r requirements.txt
 
 # Static files
 python manage.py collectstatic --noinput
+``
--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-cd backend
-
-# Migrations au démarrage (runtime)
 python manage.py migrate --noinput
 
-# Création superuser uniquement si activée
 if [ "$CREATE_SUPERUSER" = "true" ]; then
-  python manage.py shell < scripts/create_superuser.py
-fi
-
-# Lancer le serveur (WSGI)
-gunicorn core.wsgi:application --bind 0.0.0.0:${PORT}
+  python manage.py
