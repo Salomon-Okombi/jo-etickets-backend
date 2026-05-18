8b8de0ef2bb58e235162b1da27f70cff95f96b6b
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-05-18 22:53:51 +0200

    fix setting 2

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Migrations au runtime
+echo " Migration DB..."
 python manage.py migrate --noinput
 
-# Création superuser automatique (optionnel)
-if [ "$CREATE_SUPERUSER" = "true" ]; then
-  python manage.py shell < scripts/create_superuser.py
-fi
+echo " Création admin..."
+python manage.py createadmin || true
 
-# Lancer serveur Django
+echo " Collect static..."
+python manage.py collectstatic --noinput
+
+echo " Démarrage serveur..."
 gunicorn core.wsgi:application --bind 0.0.0.0:${PORT}
---
+++ users/mangement/commands/createadmin.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Créer un admin automatiquement"
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = "admin"
+        email = "admin@test.com"
+        password = "Admin123!"
+
+        if not User.objects.filter(username=username).exists():
+            user = User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+            )
+            self.stdout.write(self.style.SUCCESS(" Admin créé"))
+        else:
+            self.stdout.write(self.style.WARNING(" Admin déjà existant"))
