c2475115ed7063de0beea0a5a2a21d71f3d4b542
Bienvenu Salomon OKOMBI <salomon.okombi@gmail.com>
2026-05-19 10:56:51 +0200

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo " Migration DB..."
+echo "  Démarrage backend..."
+
+echo "  Migration DB..."
 python manage.py migrate --noinput
 
-#echo " Création admin..."
-#python manage.py createadmin || true
-
-echo " Collect static..."
+echo "  Collect static..."
 python manage.py collectstatic --noinput
 
-echo " Démarrage serveur..."
-gunicorn core.wsgi:application --bind 0.0.0.0:${PORT}
-``
+echo "  Lancement Gunicorn..."
+gunicorn core.wsgi:application --bind 0.0.0.0:$PORT --workers=2 --timeout 60
